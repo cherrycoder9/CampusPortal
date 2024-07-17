bad5d72f56e2f3dd60e3ff75313d6536dd01d96b
cherrycoder9 <cherrycoder9@gmail.com>
2024-07-17 23:38:43 +0900

    Initial commit

---
+++ src/main/java/com/campusportal/CampusPortalApplication.java
@@ -0,0 +1,13 @@
+package com.campusportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusPortalApplication.class, args);
+    }
+
+}
